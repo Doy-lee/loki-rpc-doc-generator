e9c303bd3abab8952d3f2d98bda1dc86db0de060
doyle <doylet@protonmail.com>
2020-11-18 23:03:18 +1100

    Undo commenting out some code

--- Code/build.bat
+++ Code/build.bat
@@ -16,10 +16,10 @@ set wallet_files=%wallet_files% wallet2.h
 set wallet_files=%wallet_files% wallet_rpc_server.h
 set wallet_files=%wallet_files% wallet_rpc_server_commands_defs.h
 
-REM echo title: Loki.network Documentation ^| Loki and Monero Daemon RPC Guide > DaemonRPCBeta.md
-REM echo description: This is a list of the daemon RPC calls, their inputs and outputs, and examples of each automatically generated. This guide can be used by coins who share a codebase with Loki and Monero. >> DaemonRPCBeta.md
-REM echo.>> DaemonRPCBeta.md
-REM ..\Bin\LokiRPCDocGenerator.exe %daemon_files% >> DaemonRPCBeta.md
+echo title: Loki.network Documentation ^| Loki and Monero Daemon RPC Guide > DaemonRPCBeta.md
+echo description: This is a list of the daemon RPC calls, their inputs and outputs, and examples of each automatically generated. This guide can be used by coins who share a codebase with Loki and Monero. >> DaemonRPCBeta.md
+echo.>> DaemonRPCBeta.md
+..\Bin\LokiRPCDocGenerator.exe %daemon_files% >> DaemonRPCBeta.md
 
 echo title: Loki.network Documentation ^| Loki and Monero Wallet RPC Guide > WalletRPCBeta.md
 echo description: This is a list of the wallet RPC calls, their inputs and outputs, and examples of each automatically updated. This guide can be used by coins who share a codebase with Loki and Monero. >> WalletRPCBeta.md
